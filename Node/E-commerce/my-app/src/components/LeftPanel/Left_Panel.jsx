55520c8aa7b9a4a36b62467cab1f2fbedeb16caf
Eva2992 <nilufabegum487@gmail.com>
2025-11-03 01:24:35 +0600

    Adding scrrenshot

--- Node/E-commerce/my-app/src/components/LeftPanel/Left_Panel.jsx
+++ Node/E-commerce/my-app/src/components/LeftPanel/Left_Panel.jsx
@@ -4,9 +4,11 @@ import ProductGrid from './Products/ProductGrid';
 export function Left_Panel() { //  import with curly braces in App.jsx
     return (
         <div className="w-full lg:w-full p-4 lg:p-6">
-
+          <div className="w-full bg-teal-500 p-2 rounded-lg " >
         <h1 className="text-3xl lg:text-4xl font-bold text-gray-800 mb-6">
-          Simple E-Commerce</h1>
+          Gadgets Hub</h1>
+          <p className="text-grey-800 font-bold ">Your one-stop shop for the latest and greatest in tech!</p>
+          </div>
         <FilterSection/>
         <ProductGrid/>
         
--- Node/E-commerce/my-app/src/components/LeftPanel/filter_section.jsx
+++ Node/E-commerce/my-app/src/components/LeftPanel/filter_section.jsx
@@ -6,11 +6,11 @@ export function FilterSection() {
 
 
 return (
-    <div className="mb-6">
+    <div className="mb-6 py-4 ">
     <div className="flex flex-wrap justify-center gap-2">
        { categories.map( (category) => (
             <button key = {category} 
-            className="bg-emerald-500 text-white  hover:bg-emerald-800 hover:text-white  px-4 py-2 rounded-md transition"
+            className="bg-emerald-500 text-white  hover:bg-emerald-800 hover:text-white  px-7 py-2 rounded-md transition"
             > {category}</button>
         )) }
 
@@ -23,7 +23,7 @@ return (
         
     </div>
 
-        <div className="flex justify-start mt-8">
+        <div className="flex justify-start mt-8 ">
             <button className="bg-red-500 text-white px-4 py-3 rounded-md hover:bg-red-700 transition">
                 Clear Filters</button>
         </div>
